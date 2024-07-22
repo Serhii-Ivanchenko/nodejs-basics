9e545658981cb54e243e32390ed69c1c59d28746
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-22 19:49:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ const PORT = 3000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello world!',
+  });
+});
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
   });
 });
 
