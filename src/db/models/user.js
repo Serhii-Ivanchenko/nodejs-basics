88093fbcdf283ffb4cd2db96d6f90997be699826
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-08-13 16:16:55 +0300

    add registration, login, logout

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,5 +1,4 @@
-const { Schema, model } = require('mongoose');
-
+import { Schema, model } from 'mongoose';
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
@@ -11,5 +10,11 @@ const userSchema = new Schema(
     versionKey: false,
   },
 );
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
 
 export const UserCollection = model('users', userSchema);
